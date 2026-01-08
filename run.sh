34f916a98b5dd45b4be9a4f7800edb2ad42a33d1
huzheyuan <93635197+15197580192@users.noreply.github.com>
2026-01-08 15:27:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 设置进程数
-NP=7
+# 设置进程数(1*主进程+节点个数*从进程)
+NP=9
 
 # 检查是否提供了程序路径作为参数
 if [ -z "$1" ]; then
@@ -18,7 +18,7 @@ echo "执行程序：$MPI_PROGRAM"
 start_time=$(date +%s%3N)  # 获取当前时间戳，精确到毫秒（毫秒表示）
 
 # 捕获 mpirun 输出（标准输出和标准错误都捕获）
-mpi_output=$(mpirun -np $NP --allow-run-as-root --oversubscribe $MPI_PROGRAM 2>&1)
+mpi_output=$(mpirun -np $NP $MPI_PROGRAM 2>&1)
 
 end_time=$(date +%s%3N)  # 获取当前时间戳，精确到毫秒
 
@@ -31,9 +31,6 @@ execution_time_sec=$(echo "scale=3; $execution_time / 1000" | bc)
 # 使用 printf 格式化输出，总是保留三位小数
 printf "总共耗时: %.3f 秒\n" $execution_time_sec
 
-# 去除 mpi_output 的第一行(No protocol specified)
-mpi_output_no_first_line=$(echo "$mpi_output" | sed '1d')
-
-# 输出去除第一行后的 MPI 程序输出
+# MPI 程序输出
 echo "查询结果："
-echo "$mpi_output_no_first_line"
+echo "$mpi_output"
