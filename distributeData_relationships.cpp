08ea2704fe791acfdb050125ba870438bbceed7d
huzheyuan <93635197+15197580192@users.noreply.github.com>
2026-01-21 00:30:51 +0800

    cc merged edge label

--- distributeData_relationships.cpp
+++ distributeData_relationships.cpp
@@ -7,12 +7,64 @@
 #include <sstream>
 #include <sys/io.h>
 #include <dirent.h>
+#include <cctype>    // 用于 isspace, isdigit
+#include <climits>   // 添加这个头文件，用于 INT_MAX, INT_MIN
+#include <cstdlib>   // 用于 strtol
+#include <cerrno>    // 用于 errno
 using namespace std;
 
 vector<vector<int>> conn_list;
-vector<string> label_list;
+// vector<string> label_list;// todo :修改为vector<vector<string>>
+vector<set<string>> label_list;
 vector<vector<int>> connected_component_list;
 vector<string> relationship_line;
+
+int safe_stoi(const string& str) {
+    if (str.empty()) {
+        throw invalid_argument("空字符串");
+    }
+    
+    // 移除首尾空白字符
+    size_t start = str.find_first_not_of(" \t\n\r");
+    if (start == string::npos) {
+        // 字符串全是空白字符
+        throw invalid_argument("只有空白字符");
+    }
+    
+    size_t end = str.find_last_not_of(" \t\n\r");
+    string clean_str = str.substr(start, end - start + 1);
+    
+    if (clean_str.empty()) {
+        throw invalid_argument("清理后为空字符串");
+    }
+    
+    // 检查是否全为数字（允许开头有正负号）
+    size_t check_start = 0;
+    if (clean_str[0] == '+' || clean_str[0] == '-') {
+        check_start = 1;
+    }
+    
+    for (size_t i = check_start; i < clean_str.size(); ++i) {
+        if (!isdigit(static_cast<unsigned char>(clean_str[i]))) {
+            throw invalid_argument("包含非数字字符: " + clean_str);
+        }
+    }
+    
+    // 使用 strtol 进行更安全的转换
+    char* endptr;
+    errno = 0;
+    long result = strtol(clean_str.c_str(), &endptr, 10);
+    
+    if (errno == ERANGE || result > INT_MAX || result < INT_MIN) {
+        throw out_of_range("数值超出范围: " + clean_str);
+    }
+    
+    if (*endptr != '\0') {
+        throw invalid_argument("转换未完成: " + clean_str);
+    }
+    
+    return static_cast<int>(result);
+}
 
 void Stringsplit(string str, const char split,vector<string>& res)
 {
@@ -60,7 +112,7 @@ std::string toLowercase(std::string str) {
 }
 
 int main(){
-    ifstream fin1("./output/new_SF0.1/partition_result_all_6_v2.txt");
+    ifstream fin1("./output/new_SF10/partition_result_all_8_v2.txt");
     string line;
     int line_cnt = 0;
     while(getline(fin1, line)){
@@ -83,20 +135,26 @@ int main(){
         conn_list.push_back(cc_id);
     }
 
-    ifstream fin2("./output/new_SF0.1/connected_label_v4.txt");
+    ifstream fin2("./output/new_SF10/connected_label_v4.txt");
     string cc_line;
-    label_list.push_back("NULL");
+    label_list.push_back({"NULL"});
     while(getline(fin2, cc_line)){
         int pos = cc_line.find(":");
-        string cc_label = cc_line.substr(pos + 1);
-        pos = cc_label.find_last_of('/');
-        int pos1 = cc_label.find_last_of('.');
-        // std::cout<<cc_label.substr(pos+1,pos1-pos-1)<<std::endl;
-        cc_label = cc_label.substr(pos + 1, pos1 - pos - 1);
-        label_list.push_back(cc_label);
+        // string cc_label = cc_line.substr(pos + 1);// todo:这里改为集合
+        vector<string> cc_labels;
+        Stringsplit(cc_line.substr(pos + 1),'|',cc_labels);
+        set<string> la;//存放分割后的labels
+        for(string cc_label:cc_labels){    
+            pos = cc_label.find_last_of('/');
+            int pos1 = cc_label.find_last_of('.');
+            // std::cout<<cc_label.substr(pos+1,pos1-pos-1)<<std::endl;
+            cc_label = cc_label.substr(pos + 1, pos1 - pos - 1);
+            la.insert(cc_label);
+        }
+        label_list.push_back(la);
     }
 
-    ifstream fin3("./output/new_SF0.1/region_component_v4.txt");
+    ifstream fin3("./output/new_SF10/region_component_v4.txt");
     string node_line;
     int cc_cnt = 0;
     vector<int> tini;
@@ -116,7 +174,7 @@ int main(){
 	connected_component_list.push_back(node_id);
     }
 
-    ifstream fin4("./output/new_SF0.1/new_output_relationships.txt");
+    ifstream fin4("./output/new_SF10/new_output_relationships.txt");
     string r_line;
     long long r_cnt = 0;
     while (getline(fin4, r_line)) {
@@ -125,7 +183,7 @@ int main(){
     }
 
     int now_partition_id = 0;
-    string file_path = "./output/new_SF0.1/new_output_relationships";
+    string file_path = "./output/new_SF10/new_output_relationships";
     vector<string> my_file, file_list;
 	string need_extension = "txt";
 	get_need_file(file_path, my_file, need_extension);
@@ -138,12 +196,13 @@ int main(){
         tline = tline.substr(0, pos);
         file_list.push_back(tline);
     }
-    while (now_partition_id < 6){
+    while (now_partition_id < 8){
         for (int i = 0; i < my_file.size(); i++){
             set<int> conn_id_list;
             for (int j = 0; j < conn_list[now_partition_id].size(); j++){
                 int bh = conn_list[now_partition_id][j];
-                if (label_list[bh] == file_list[i]){
+                // if (label_list[bh] == file_list[i]){// todo:修改为集合
+                if (label_list[bh].find(file_list[i])!=label_list[bh].end()){
                     for (int k = 0; k < connected_component_list[bh].size(); k++){
                         conn_id_list.insert(connected_component_list[bh][k]);
                     }
@@ -171,7 +230,8 @@ int main(){
                     label2 = tline.substr(0, pos);
                     label1 = toLowercase(label1);
                     label2 = toLowercase(label2);
-                    string fout_name = "./output/new_SF0.1/partition_txt/partition_" + to_string(now_partition_id) + "/" + label1 + "_" + file_list[i] + "_" + label2 + ".txt";
+                    // node1_filelist[i]_node2 comment_hasCreator_person
+                    string fout_name = "./output/new_SF10/partition_txt/partition_" + to_string(now_partition_id) + "/" + label1 + "_" + file_list[i] + "_" + label2 + ".txt";
                     tout1.open(fout_name);
                     has_init = 1;
                 }
@@ -189,7 +249,7 @@ int main(){
                     out_file_cnt++;
                     label1 = tlabel1;
                     label2 = tlabel2;
-                    string fout_name = "./output/new_SF0.1/partition_txt/partition_" + to_string(now_partition_id) + "/" + label1 + "_" + file_list[i] + "_" + label2 + ".txt";
+                    string fout_name = "./output/new_SF10/partition_txt/partition_" + to_string(now_partition_id) + "/" + label1 + "_" + file_list[i] + "_" + label2 + ".txt";
                     if (out_file_cnt == 2) {
                         tout2.open(fout_name);
                     }
